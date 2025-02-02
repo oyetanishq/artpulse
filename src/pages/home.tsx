f0e65a6aba187511d8bc97afbcc0445868bfb829
Tanishq Singh <hi@tanishqsingh.com>
2025-02-03 01:24:55 +0530

    canvas offline syns

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,25 @@
+import { useRef } from "react";
+import { ReactSketchCanvas } from "react-sketch-canvas";
+
 const Home = () => {
+	const canvas = useRef(null);
+
 	return (
-		<main className="h-full flex justify-center items-center">
-			<p className="underline underline-offset-4 cursor-pointer">home</p>
+		<main className="h-full flex flex-col justify-center items-center">
+			<ReactSketchCanvas
+				className="!border !rounded-sm !bg-white"
+				width="100%"
+				height="50%"
+				strokeWidth={1}
+				strokeColor="black"
+				onStroke={(e) => {
+					if (e.paths.length === 1) return;
+					if (!canvas.current) return;
+
+					(canvas.current as any).loadPaths(e);
+				}}
+			/>
+			<ReactSketchCanvas ref={canvas} className="!border !rounded-sm" width="100%" height="50%" strokeWidth={1} />
 		</main>
 	);
 };
