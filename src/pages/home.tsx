cf77ef453547f53815bba41e2b3192c7eb253cb7
Tanishq Singh <hi@tanishqsingh.com>
2025-02-03 08:59:33 +0530

    conncted to socket

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,25 +1,47 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
+import { randomColor } from "@/lib/random-color";
 
 const Home = () => {
+	const [socket, setSocket] = useState<WebSocket | null>(null);
+	const [color] = useState(randomColor());
 	const canvas = useRef(null);
 
+	useEffect(() => {
+		const ws = new WebSocket(import.meta.env.VITE_SOCKET_URL);
+		ws.onopen = () => console.log("connected");
+		ws.onclose = () => console.log("closed");
+		ws.onmessage = ({ data }) => {
+			if (!canvas.current) return;
+
+			try {
+				(canvas.current as any).loadPaths(JSON.parse(data).message);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		setSocket(ws);
+
+		return () => ws.close();
+	}, []);
+
+	if (!socket) return <main className="h-full flex justify-center items-center">connecting...</main>;
+
 	return (
-		<main className="h-full flex flex-col justify-center items-center">
+		<main className="h-full flex justify-center items-center">
 			<ReactSketchCanvas
 				className="!border !rounded-sm !bg-white"
 				width="100%"
-				height="50%"
+				height="100%"
+				ref={canvas}
 				strokeWidth={1}
-				strokeColor="black"
+				strokeColor={color}
 				onStroke={(e) => {
 					if (e.paths.length === 1) return;
-					if (!canvas.current) return;
-
-					(canvas.current as any).loadPaths(e);
+					socket.send(JSON.stringify({ type: "sendtoall", message: e }));
 				}}
 			/>
-			<ReactSketchCanvas ref={canvas} className="!border !rounded-sm" width="100%" height="50%" strokeWidth={1} />
 		</main>
 	);
 };
